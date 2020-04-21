b373da198ac706c27d173f6c66252fb5cd55f03f
Joao Rocha <jr007silva1306@gmail.com>
2020-04-21 22:51:42 +0100

    Updates

---
+++ precache-manifest.29f491b44d5938a34f5e576a8152d0dd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abf9303abda5e3187770a324ba8a84dd",
+    "url": "/personal/index.html"
+  },
+  {
+    "revision": "17b39b69be5ddfb7cf3b",
+    "url": "/personal/static/css/main.b807fb04.chunk.css"
+  },
+  {
+    "revision": "fec08306b3f1504ec5b4",
+    "url": "/personal/static/js/2.b755412e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/personal/static/js/2.b755412e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17b39b69be5ddfb7cf3b",
+    "url": "/personal/static/js/main.ecfe346b.chunk.js"
+  },
+  {
+    "revision": "b6628ab84b56e94c7226",
+    "url": "/personal/static/js/runtime-main.41eb645a.js"
+  },
+  {
+    "revision": "e0da3324f88b70ba088f7a25fabbe646",
+    "url": "/personal/static/media/header-background.e0da3324.jpg"
+  },
+  {
+    "revision": "8141a5086591fb1cf93068c87df50bce",
+    "url": "/personal/static/media/profile.8141a508.jpg"
+  }
+]);
